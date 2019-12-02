8bfd3c151b8789a1180f0bb152517fb01defb0aa
Valentin Berger <Valentin.berger38@gmail.com>
2019-12-02 11:10:22 +0100

    :construction: Started part 1 of TP2

--- TP2/tp2.m
+++ TP2/tp2.m
@@ -6,8 +6,26 @@ pkg load image
 
 # 1 - Arbre à chats
 
-[chat, map, alpha] = imread("data/chat.jpg");
-[arbre, map2, alpha2] = imread("data/arbre.jpg");
+[chat, map_chat, alpha_chat] = imread("data/chat.jpg");
+[arbre, map_arbre, alpha_arbre] = imread("data/arbre.jpg");
+
+chat_transp = chat;
+%chat_transp(:, :, 2) = 0;
+%{
+for x = 1:size(chat_transp, 1)
+	for y = 1:size(chat_transp, 2)
+		if chat_transp(x, y, 1) == 106 && chat_transp(x, y, 2) == 182 && chat_transp(x, y, 3) == 107
+			chat_transp(x, y, 1) = 255;
+			chat_transp(x, y, 2) = 255;
+			chat_transp(x, y, 3) = 255;
+		endif
+	endfor
+endfor
+%}
+chat_transp(chat_transp(:, :, 1) == 106 && chat_transp(:, :, 2) == 182 && chat_transp(:, :, 3) == 107) = [255 255 255];
+f = figure;
+imshow(chat_transp);
+title("Chat sans fond vert");
 
 # 2 - Segmentation
 
