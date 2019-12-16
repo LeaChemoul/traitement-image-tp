d7ff5b31da2a126c57a01330faa97d4e717a0f1a
Valentin Berger <Valentin.berger38@gmail.com>
2019-12-16 11:17:08 +0100

    :sparkles: Finish question 1 of part 1 of TP3

--- TP3/TP3.md
+++ TP3/TP3.md
@@ -9,4 +9,25 @@
 
 ### 1 - Filtres passe haut
 
+Pour réaliser une convolution 2D sur les images, nous utilisons la fonction Matlab `conv2`.
+
+```matlab
+% Convolution 2D de l'image domino avec le masque de convolution H3 :
+dominos_conv_h3 = conv2(dominos, H3);
+```
+
+Les bords ne sont pas très bien traité avec cette fonction : en effet, `domino` et `lena` ont des bords noirs, ce qui signifie que la convolution a commencé sur les bordure, sans *offset*. Pour résoudre ce problème, nous utilisons le paramètre `shape='same'` :
+
+```matlab
+dominos_conv_h3 = conv2(dominos, H3, 'same');
+```
+
+![Convolution 2D des images avec les masques H3, H4 et H5](output/all_conv2_h3_h4_h5.png)
+
+![Convolution 2D de monde et sa transposé avec les masques H3, H4](output/monde_transposed_conv2_h1_h2.png)
+
 ### 2 - Filtres passe bas
+
+![Convolution 2D des images avec les masques H1 et H2](output/all_conv2_h1_h2.png)
+
+![Convolution 2D de monde avec les masques H1 et H2](output/monde_conv2_h1_h2.png)
--- TP3/tp3.m
+++ TP3/tp3.m
@@ -14,17 +14,25 @@ H5 = [0, 1, 0; 1, -4, 1; 0, 1, 0];
 [monde, map_monde, alpha_monde] = imread("data/monde.png");
 [lena, map_lena, alpha_lena] = imread("../TP2/data/Lena_nb.jpg");
 
-dominos_conv_h3 = conv2(dominos, H3);
-monde_conv_h3 = conv2(monde, H3);
-lena_conv_h3 = conv2(lena, H3);
+# See https://fr.mathworks.com/help/matlab/ref/conv2.html
 
-dominos_conv_h4 = conv2(dominos, H4);
-monde_conv_h4 = conv2(monde, H4);
-lena_conv_h4 = conv2(lena, H4);
+function img_conv = conv(img, mask)
+	img_conv = uint8(conv2(img, mask, "same"));
+endfunction
 
-dominos_conv_h5 = conv2(dominos, H5);
-monde_conv_h5 = conv2(monde, H5);
-lena_conv_h5 = conv2(lena, H5);
+function [dominos_conv, monde_conv, lena_conv] = convImages(mask)
+	global dominos;
+	global monde;
+	global lena;
+	dominos_conv = conv(dominos, mask);
+	monde_conv = conv(monde, mask);
+	lena_conv = conv(lena, mask);
+endfunction
+
+% Plot all convolutions of images with convolution masks
+[dominos_conv_h3, monde_conv_h3, lena_conv_h3] = convImages(H3);
+[dominos_conv_h4, monde_conv_h4, lena_conv_h4] = convImages(H4);
+[dominos_conv_h5, monde_conv_h5, lena_conv_h5] = convImages(H5);
 
 figure;
 subplot(3, 3, 1);
@@ -56,32 +64,32 @@ title("monde convoluted with H5");
 f = subplot(3, 3, 9);
 imshow(lena_conv_h5);
 title("lena convoluted with H5");
-saveas(f, "output/conv2_all_h3_h4_h5.png");
+saveas(f, "output/all_conv2_h3_h4_h5.png");
 
+% Plot convolution of "monde" (normal and transposed) with H3 and H4
 figure;
-subplot(1, 3, 1);
+subplot(2, 2, 1);
 imshow(monde_conv_h3);
 title("monde convoluted with H3");
-subplot(1, 3, 2);
+subplot(2, 2, 2);
 imshow(monde_conv_h4);
 title("monde convoluted with H4");
-f = subplot(1, 3, 3);
-imshow(monde_conv_h5);
-title("monde convoluted with H5");
-saveas(f, "output/monde_conv2_h3_h4_h5.png");
+subplot(2, 2, 3);
+imshow(conv2(monde', H3, "same"));
+title("monde transposed convoluted with H3");
+f = subplot(2, 2, 4);
+imshow(conv2(monde', H4, "same"));
+title("monde transposed convoluted with H4");
+saveas(f, "output/monde_transposed_conv2_h3_h4.png");
 
 # 2 - Filtres passe haut
 
 H1 = (1/9) * ones([3, 3]);
 H2 = (1/6) * [1, 2, 1; 2, 4, 2; 1, 2, 1];
 
-dominos_conv_h1 = conv2(dominos, H1);
-monde_conv_h1 = conv2(monde, H1);
-lena_conv_h1 = conv2(lena, H1);
-
-dominos_conv_h2 = conv2(dominos, H2);
-monde_conv_h2 = conv2(monde, H2);
-lena_conv_h2 = conv2(lena, H2);
+% Plot all convolution of images with H1 and H2
+[dominos_conv_h1, monde_conv_h1, lena_conv_h1] = convImages(H1);
+[dominos_conv_h2, monde_conv_h2, lena_conv_h2] = convImages(H2);
 
 figure;
 subplot(2, 3, 1);
@@ -102,7 +110,9 @@ title("monde convoluted with H2");
 f = subplot(2, 3, 6);
 imshow(lena_conv_h2);
 title("lena convoluted with H2");
+saveas(f, "output/all_conv2_h1_h2.png");
 
+% Plot convolution of "monde" with H1 and H2
 figure;
 subplot(1, 2, 1);
 imshow(monde_conv_h1);
@@ -115,4 +125,4 @@ saveas(f, "output/monde_conv2_h1_h2.png");
 
 
 % Comment the following line to keep the images displayed during execution.
-%close all hidden;
+close all hidden;
