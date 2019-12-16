970516d3f975c9592a485327227ae818a29981c9
Valentin Berger <Valentin.berger38@gmail.com>
2019-12-16 09:28:48 +0100

    :tada: Init the TP3
    * Added all required files to begin the TP3.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 [TP1](TP1/TP1.md)
+
 [TP2](TP2/TP2.md)
+
+[TP3](TP3/TP3.md)
---
+++ TP3/tp3.m
@@ -0,0 +1,12 @@
+# Traitement d'Image
+
+pkg load image
+
+# TP3
+
+# 1 - Filtres passe haut
+
+
+
+# 2 - Filtres passe haut
+
