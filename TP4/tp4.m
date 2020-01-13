937063b615314635ef311c43e1e860aa90f55eaa
Lea <lea_chemoul@orange.fr>
2020-01-13 08:36:18 +0100

    :sparkles: close & open

--- TP4/tp4.m
+++ TP4/tp4.m
@@ -24,6 +24,21 @@ title("Cameraman dilated");
 
 saveas(f, "output/cameraman_erode_dilate.png");
 
+cameraman_fermeture = imerode(cameraman_dilate, strel("square", 3), 'same');
+cameraman_ouverture = imdilate(cameraman_erode, strel("square", 3), 'same');
+
+figure;
+subplot(1, 3, 1);
+imshow(cameraman);
+title("Cameraman original");
+subplot(1, 3, 2);
+imshow(cameraman_fermeture);
+title("Cameramn closed");
+f = subplot(1, 3, 3);
+imshow(cameraman_ouverture);
+title("Cameraman opened");
+
+saveas(f, "output/cameraman_closed_opened.png");
 
 # 2 - Contours
 
