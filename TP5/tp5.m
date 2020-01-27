4fa25586620b3b20b0b242292f1711f8ebfafc84
Valentin Berger <Valentin.berger38@gmail.com>
2020-01-27 09:24:01 +0100

    :sparkles: Add output of part 1 for TP5

--- TP5/tp5.m
+++ TP5/tp5.m
@@ -16,18 +16,60 @@ function transformed = fourier(img)
 	transformed = fftshift(transformed);
 endfunction
 
-sinus_fft = fourier(sinus);
-arg_sinus = arg(sinus_fft);
-phase_sinus = abs(sinus_fft);
+function [transformed, magnitude, phase] = ftcomponent(img)
+	transformed = fourier(img);
+	magnitude = abs(transformed);
+	phase = arg(transformed);
+endfunction
 
-figure;
-subplot(1, 2, 1);
+[sinus_fft, magnitude_sinus, arg_sinus] = ftcomponent(sinus);
+[monde_fft, magnitude_monde, arg_monde] = ftcomponent(monde);
+[dominos_fft, magnitude_dominos, arg_dominos] = ftcomponent(dominos);
+[lena_fft, magnitude_lena, arg_lena] = ftcomponent(lena);
+
+figure(1, "position", [0, 0, 1920, 1000]);
+
+subplot(4, 3, 1);
 imshow(sinus);
 title("Sinus");
-f = subplot(1, 2, 2);
-imagesc(abs(sinus_fft));
-title("Transformation de Fourier de sinus");
-saveas(f, "output/sinus_fft.png");
+subplot(4, 3, 2);
+imagesc(magnitude_sinus);
+title("Module de sinus");
+subplot(4, 3, 3);
+imagesc(arg_sinus);
+title("Phase de sinus");
+
+subplot(4, 3, 4);
+imshow(monde);
+title("Monde");
+subplot(4, 3, 5);
+imagesc(magnitude_monde);
+title("Module de monde");
+subplot(4, 3, 6);
+imagesc(arg_monde);
+title("Phase de monde");
+
+subplot(4, 3, 7);
+imshow(dominos);
+title("Dominos");
+subplot(4, 3, 8);
+imagesc(magnitude_dominos);
+title("Module de dominos");
+subplot(4, 3, 9);
+imagesc(arg_dominos);
+title("Phase de dominos");
+
+subplot(4, 3, 10);
+imshow(lena);
+title("Lena");
+subplot(4, 3, 11);
+imagesc(magnitude_lena);
+title("Module de lena");
+f = subplot(4, 3, 12);
+imagesc(arg_lena);
+title("Phase de lena");
+
+saveas(f, "output/fourier_components.png");
 
 # 2 - Module et phase
 
