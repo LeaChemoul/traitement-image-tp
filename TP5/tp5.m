94486aee06e464e59475f808ec568dba0a18c0e0
Valentin Berger <Valentin.berger38@gmail.com>
2020-01-27 09:50:28 +0100

    :bug: Fix magnitude in part 1

--- TP5/tp5.m
+++ TP5/tp5.m
@@ -14,20 +14,32 @@ pkg load image
 [beach, map_ebach, alpha_beach] = imread("data/beach.tif");
 
 function transformed = fourier(img)
-	transformed = fft2(img);
+	transformed = fft2(img); % , 2^nextpow2(200), 2^nextpow2(600)
 	transformed = fftshift(transformed);
 endfunction
 
-function [transformed, magnitude, phase] = ftcomponent(img)
+function p = choosePixel(pixel)
+	threshold = 30000;
+	if pixel < threshold
+		p = pixel;
+	else
+		p = threshold;
+	endif
+endfunction
+
+function [transformed, magnitude, phase] = ftcomponent(img, maxMagnitude = false)
 	transformed = fourier(img);
 	magnitude = abs(transformed);
+	if maxMagnitude
+		magnitude = arrayfun(@choosePixel, magnitude);
+	endif
 	phase = arg(transformed);
 endfunction
 
-[sinus_fft, magnitude_sinus, arg_sinus] = ftcomponent(sinus);
-[monde_fft, magnitude_monde, arg_monde] = ftcomponent(monde);
-[dominos_fft, magnitude_dominos, arg_dominos] = ftcomponent(dominos);
-[lena_fft, magnitude_lena, arg_lena] = ftcomponent(lena);
+[sinus_fft, magnitude_sinus, arg_sinus] = ftcomponent(sinus, true);
+[monde_fft, magnitude_monde, arg_monde] = ftcomponent(monde, true);
+[dominos_fft, magnitude_dominos, arg_dominos] = ftcomponent(dominos, true);
+[lena_fft, magnitude_lena, arg_lena] = ftcomponent(lena, true);
 
 figure(1, "position", [0, 0, 1920, 1000]);
 
@@ -36,6 +48,7 @@ imshow(sinus);
 title("Sinus");
 subplot(4, 3, 2);
 imagesc(magnitude_sinus);
+colorbar();
 title("Module de sinus");
 subplot(4, 3, 3);
 imagesc(arg_sinus);
@@ -46,6 +59,7 @@ imshow(monde);
 title("Monde");
 subplot(4, 3, 5);
 imagesc(magnitude_monde);
+colorbar();
 title("Module de monde");
 subplot(4, 3, 6);
 imagesc(arg_monde);
@@ -56,6 +70,7 @@ imshow(dominos);
 title("Dominos");
 subplot(4, 3, 8);
 imagesc(magnitude_dominos);
+colorbar();
 title("Module de dominos");
 subplot(4, 3, 9);
 imagesc(arg_dominos);
@@ -66,6 +81,7 @@ imshow(lena);
 title("Lena");
 subplot(4, 3, 11);
 imagesc(magnitude_lena);
+colorbar();
 title("Module de lena");
 f = subplot(4, 3, 12);
 imagesc(arg_lena);
