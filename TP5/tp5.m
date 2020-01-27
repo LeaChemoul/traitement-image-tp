0490960f0584675e7489afdaea220ac0fa5dc285
Lea <lea_chemoul@orange.fr>
2020-01-27 09:03:02 +0100

    :sparkles: Fourier of beach and manhattan

--- TP5/tp5.m
+++ TP5/tp5.m
@@ -10,6 +10,8 @@ pkg load image
 [monde, map_monde, alpha_monde] = imread("data/monde.png");
 [dominos, map_dominos, alpha_dominos] = imread("data/dominos.png");
 [lena, map_lena, alpha_lena] = imread("data/Lena_nb.jpg");
+[manhattan, map_manhattan, alpha_manhattan] = imread("data/manhattan.tif");
+[beach, map_ebach, alpha_beach] = imread("data/beach.tif");
 
 function transformed = fourier(img)
 	transformed = fft2(img);
@@ -31,7 +33,26 @@ saveas(f, "output/sinus_fft.png");
 
 # 2 - Module et phase
 
+manhattan_fft = fourier(manhattan);
+beach_fft = fourier(beach);
 
+figure;
+subplot(1, 2, 1);
+imshow(manhattan);
+title("Manhattan");
+f = subplot(1, 2, 2);
+imagesc(abs(manhattan_fft));
+title("Transformation de Fourier de Manhattan");
+saveas(f, "output/manhattan_fft.png");
+
+figure;
+subplot(1, 2, 1);
+imshow(beach);
+title("Beach");
+f = subplot(1, 2, 2);
+imagesc(abs(beach_fft));
+title("Transformation de Fourier de Beach");
+saveas(f, "output/beach_fft.png");
 
 # 3 - Filtrage
 
